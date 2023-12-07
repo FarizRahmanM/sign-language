0b97b4148c4bc2355aaaff3ac819469b3e7c0275
FarizRahmanM <95173571+FarizRahmanM@users.noreply.github.com>
2023-12-07 13:58:43 +0700

    Add files via upload

---
+++ tubes4/app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, Response, request, redirect, url_for, send_from_directory
+import cv2
+import numpy as np
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+model = load_model("sign_language6")
+
+def classify(image):
+    image = cv2.resize(image, (28, 28))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    proba = model.predict(image)
+    idx = np.argmax(proba)
+    return alphabet[idx]
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, img = cap.read()
+        img = cv2.flip(img, 1)
+        top, right, bottom, left = 75, 350, 300, 590
+        roi = img[top:bottom, right:left]
+        roi = cv2.flip(roi, 1)
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        alpha = classify(gray)
+        cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, alpha, (0, 130), font, 5, (0, 0, 255), 2)
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            image = cv2.imread(file_path)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.resize(gray, (28, 28))
+            alpha = classify(gray)
+
+            result_image = url_for('uploaded_file', filename=filename)
+            print(result_image)  # Cek nilai result_image di console/terminal
+
+            os.remove(file_path)  # Remove the uploaded file
+
+            return render_template('result.html', prediction=alpha, result_image=result_image)
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    return upload_file()
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
